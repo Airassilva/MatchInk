22cd1bd0279280960025ca931834fbfe3c2bec18
Airassilva <114684137+Airassilva@users.noreply.github.com>
2023-10-01 21:45:18 +0000

    ajustes

--- MatchInk-Production/assets/util.js
+++ MatchInk-Production/assets/util.js
@@ -1,6 +1,6 @@
 //permissão para a localização
 navigator.geolocation.getCurrentPosition(function (location) {
-    appendLocation(location, 'fetched');
+    console.log(location, 'fetched');
 });
 //permissão para a notificação
 Notification.requestPermission(function (result) {
